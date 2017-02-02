f8e42d6688fb961d33e3156d1a28c4b3d13911c6
PaVM <pavboxdev@gmail.com>
2017-02-02 09:32:59 +0300

    task1a completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import Greeting from './Greeting/Greeting';
+var Skb = require('skb');
+var token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ODIyMmU5Y2E3MmJkYTAwMTJjNjkwY2IiLCJ1c2VybmFtZSI6InBhdmJveGRldkBnbWFpbC5jb20iLCJyb2xlIjoidXNlciIsImlhdCI6MTQ3ODYzNTE2Nn0.CO-mxvW67JU2Nhx4x6PjG5-_bx61GjFjMPoC7nlvCoE';
+var skb = new Skb(token);
 
-const tester = new Greeting('Tester');
-tester.hello()
+skb.taskHelloWorld('Привет, друзья)');
